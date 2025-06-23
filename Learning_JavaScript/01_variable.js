09761e26f1b084bbb035197f571c23774629a898
adeel-arshad-dev <adeelarshad.developer@gmail.com>
2025-06-23 09:31:47 +0000

    variable

--- Learning_JavaScript/01_variable.js
+++ Learning_JavaScript/01_variable.js
@@ -1,2 +1,3 @@
 console.log("we will check that whether this is shown on github or not")
 console.log("and weare done with that as our code is being shown on the github")
+console.log("now my code is being shown on")
