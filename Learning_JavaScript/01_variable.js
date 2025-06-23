d565611bafbb4c87feb633730fac47a53993e766
adeel-arshad-dev <adeelarshad.developer@gmail.com>
2025-06-23 09:44:12 +0000

    JS Variables

--- Learning_JavaScript/01_variable.js
+++ Learning_JavaScript/01_variable.js
@@ -1,3 +1,22 @@
-console.log("we will check that whether this is shown on github or not")
-console.log("and weare done with that as our code is being shown on the github")
-console.log("now my code is being shown on")
+const accountId = 144553
+let accountEmail = "adeelarshad.developer@gmail.com"
+var accountPassword = "334455"
+accountCity = "Lahore"
+let accountState;
+
+// accountId = 2 // not allowed
+
+// the value will be udated as we have assigned values to variable again
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Islamabad"
+// it will show nothing
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
