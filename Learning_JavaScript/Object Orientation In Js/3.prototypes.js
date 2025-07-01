753f9f12be522e0becdec2248bcfdcd9570c8427
Adeel Arshad <adeelarshad.developer@gmail.com>
2025-07-01 17:41:12 +0000

    prototypes and Creating prototype in js

--- Learning_JavaScript/Object Orientation In Js/3.prototypes.js
+++ Learning_JavaScript/Object Orientation In Js/3.prototypes.js
@@ -1,8 +1,12 @@
-let myname="adeel   "
+let myname = "adeel   "
 // if i want that it should not count spaces but not with trim() using another function created in prototype
-Object.prototype.trulength=function(str){
-    console.log(str.trim().length);
-    
+// Object.prototype.trulength=function(str){
+//     console.log(str.trim().length);
+
+// } this can be done in best method as following
+Object.prototype.trulength = function () {
+    console.log(this.trim().length);
+
 }
 console.log(myname.length);
 
@@ -22,28 +26,28 @@ let heroPower = {
     thor: "hammer",
     spiderman: "sling",
 
-    getSpiderPower: function(){
+    getSpiderPower: function () {
         console.log(`Spidy power is ${this.spiderman}`);
     }
 }
 
-Object.prototype.hitesh = function(){
+Object.prototype.adeel = function () {
     console.log(`hitesh is present in all objects`);
 }
 
-Array.prototype.heyHitesh = function(){
+Array.prototype.heyadeel = function () {
     console.log(`Hitesh says hello`);
 }
 
-// heroPower.hitesh()
-// myHeros.hitesh()
-// myHeros.heyHitesh()
-// heroPower.heyHitesh()
+// heroPower.adeel()
+// myHeros.adeel()
+// myHeros.heyadeel()
+// heroPower.heyadeel()
 
 // inheritance
 
 const User = {
-    name: "chai",
+    name: "Adeel",
     email: "chai@google.com"
 }
 
@@ -66,13 +70,19 @@ Teacher.__proto__ = User
 // modern syntax
 Object.setPrototypeOf(TeachingSupport, Teacher)
 
-let anotherUsername = "ChaiAurCode     "
+let anotherUsername = "programmer     "
 
-String.prototype.trueLength = function(){
+String.prototype.trueLength = function () {
     console.log(`${this}`);
     console.log(`True length is: ${this.trim().length}`);
 }
 
 anotherUsername.trueLength()
-"hitesh".trueLength()
-"iceTea".trueLength()
+"Adeel".trueLength()
+"Mango".trueLength()
+
+/*
+-----------< *** important Notes ***>--------------
+#1---> if we have created a new prototype of object than we can apply  that prototype on arrays and strings. but if we have created the prototype of Arrays or strings we cannot apply that prototype to objects.
+the reason is that strings and Arrays at the end  are recognised as objects.that is the quality of javascript that it try to get parents and the parents of all in js is Object. so that is why the prototype created manually for the objects can be used for Arrays and Strings.
+*/
